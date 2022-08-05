ab3fdbf851c658c268ce8f1624f7fc532148b928
CJ <cjdegonzales@gmail.com>
2022-08-04 19:47:48 -0700

    trying this out

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+window.prompt (weeklyExpenseQuestions[0])
 
-// Your Code Here
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+for(){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; ){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; ){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
